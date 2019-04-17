daac9bb26c27d21d4a6543d3810251449aa013bc
syc0821 <syc0821@naver.com>
2019-04-17 17:31:52 +0900

    commit4

--- higitTest/src/higitTest/hi.java
+++ higitTest/src/higitTest/hi.java
@@ -2,9 +2,10 @@ package higitTest;
 
 public class hi {
 public static void main(String[] args) {
+	
 	System.out.println("안녕하세요! Test1");
 	System.out.println("안녕하세요! Test2");
 	System.out.println("안녕하세요! Test3");
 	
-}
+	}
 }
