4f39891f43f15fe502fd70dfdc8182f1765f68ca
syc0821 <syc0821@naver.com>
2019-04-17 17:39:49 +0900

    commit5

--- higitTest/src/higitTest/hi.java
+++ higitTest/src/higitTest/hi.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	System.out.println("안녕하세요! Test3");
 	System.out.println("안녕하세요! Test4");
 	System.out.println("안녕하세요! Test5");
+	System.out.println("안녕하세요! Test6");
 
-	
 	}
 }
